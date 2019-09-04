b183d64bbeefa08861fadeaf8fa8f4dc44974e8d
Rodolphe <dutchr@live.fr>
2019-09-04 15:20:14 +0200

    finish all features need style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:'products',
+    component: ProductsComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'cart',
+    component: CartComponent
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,19 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    ProductsComponent,
+    AboutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
